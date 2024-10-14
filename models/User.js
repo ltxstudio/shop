ddff36a0217b40acbba0710bb284fcbbcd2e2fb2
ltxstudio <ltxstudio.info@gmail.com>
2024-10-15 05:01:50 +0600

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,9 @@
+// models/User.js
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
